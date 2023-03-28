68894905d2e25b1e30436cabe63789cbcab4ca50
Fabien CURFS <fabien.curfs@etudiant.univ-reims.fr>
2023-03-28 17:51:18 +0200

    Tester si le user est bien un user vérifié

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Router} from "./Router";
 import {BrowserRouter, Navigate} from "react-router-dom";
+import axios from "axios";
 
 export function RequireAuth({ children }) {
     // TODO Get user from local storage
     const user = localStorage.getItem('token');
 
     if (user === null) {
-        console.log('ici bg fabinax');
         return <Navigate to="/login" replace={true}/>;
     } else {
+        axios.get('http://localhost:8000/api/users/me', {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token'),
+            }
+        })
+            .then((res) => {
+                console.log(res.data);
+                console.log('ok');
+            })
+            .catch((err) => {
+                console.log(err.response.data);
+                console.log('pas ok');
+            })
         return children;
     }
 }
