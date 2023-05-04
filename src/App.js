c155722d9e196f77e633be16fc1687cdc9c4d286
Fabien CURFS <fabien.curfs@etudiant.univ-reims.fr>
2023-05-04 08:33:20 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Router} from "./Router";
 import {BrowserRouter, Navigate} from "react-router-dom";
 import axios from "axios";
 
 export function RequireAuth({ children }) {
-    // TODO Get user from local storage
-    const user = localStorage.getItem('token');
+    const [user, setUser] = useState(null);
+    const token = localStorage.getItem('token');
 
-    if (user === null) {
-        return <Navigate to="/login" replace={true}/>;
-    } else {
-        axios.get('http://localhost:8000/api/users/me', {
-            headers: {
-                'Authorization': 'Bearer ' + localStorage.getItem('token'),
-            }
-        })
-            .then((res) => {
-                console.log(res.data);
-                console.log('ok');
-            })
-            .catch((err) => {
-                console.log(err.response.data);
-                console.log('pas ok');
-            })
-        return children;
+    useEffect(() => {
+        if (token) {
+            axios
+                .get('http://localhost:8000/api/users/me', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                })
+                .then((res) => {
+                    setUser(res.data);
+                })
+                .catch((err) => {
+                    console.log(err.response.data);
+                });
+        }
+    }, [token]);
+
+    if (!user) {
+        return <Navigate to="/login" replace={true} />;
     }
+
+    return children;
 }
 
 function App() {
