83b773d541e11dc3b2af273a4976acda9a492b13
Fabien CURFS <fabien.curfs@etudiant.univ-reims.fr>
2023-05-04 10:33:57 +0200

    push

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {Navigate} from "react-router-dom";
+import "./Home.css";
+import axios from "axios";
 
 const Home = (props) => {
 
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/users/me', {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token'),
+            }
+        })
+            .then((res) => {
+                console.log(res.data.user.email);
+                setUser(res.data.user.email);
+            })
+    }, [])
+
+    const logout = () => {
+        localStorage.clear();
+        window.location.href = "/login";
+    }
+
     return(
         <div>
-            Home page
+            <h1>Home page</h1>
+            {
+                user ? <h3>Bonjour {user}</h3> : ''
+            }
+            <span className="logout" onClick={logout}>Se déconnecter</span>
         </div>
     )
 }
